e92f4b230b6309533847abc2ecfca768778c2a6a
Micko Widi <widimicko85@gmail.com>
2021-11-28 14:30:43 +0700

    add Archive Model

--- app/Database/Migrations/2021-11-28-071649_Archive.php
+++ app/Database/Migrations/2021-11-28-071649_Archive.php
@@ -9,7 +9,7 @@ class Archive extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id'          => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
+            'id'          => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true,],
             'title'       => ['type' => 'varchar', 'constraint' => '255'],
             'document'    => ['type' => 'varchar', 'constraint' => '255'],
             'category'    => ['type' => 'varchar', 'constraint' => '255'],
---
+++ app/Models/ArchiveModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ArchiveModel extends Model {
+
+  protected $table = 'exam';
+  protected $primaryKey = 'id';
+  protected $useAutoIncrement = true;
+  protected $useTimestamps = true;
+  protected $createdField  = 'created_at';
+  protected $updatedField  = 'updated_at';
+  protected $allowedFields = ['id', 'title', 'document', 'category', 'created_at', 'updated_at'];
+
+
+}
