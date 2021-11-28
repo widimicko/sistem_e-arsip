8164bc70d981332c95c91a8a7965b40729d5e488
Micko Widi <widimicko85@gmail.com>
2021-11-28 14:25:28 +0700

    add archive migration

---
+++ app/Database/Migrations/2021-11-28-071649_Archive.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Archive extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
+            'title'       => ['type' => 'varchar', 'constraint' => '255'],
+            'document'    => ['type' => 'varchar', 'constraint' => '255'],
+            'category'    => ['type' => 'varchar', 'constraint' => '255'],
+            'created_at'  => ['type' => 'datetime'],
+            'updated_at'  => ['type' => 'datetime'],
+          ]);
+          $this->forge->addPrimaryKey('id');
+          $this->forge->createTable('archive');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('archive');
+    }
+}
