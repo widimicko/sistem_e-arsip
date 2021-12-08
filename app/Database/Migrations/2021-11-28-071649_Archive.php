e6ec72ec68be6f13f34948d601293372921ee87a
Micko Widi <widimicko85@gmail.com>
2021-12-08 11:35:32 +0700

    update migration

--- app/Database/Migrations/2021-11-28-071649_Archive.php
+++ app/Database/Migrations/2021-11-28-071649_Archive.php
@@ -8,20 +8,30 @@ class Archive extends Migration
 {
     public function up()
     {
+          
+        $this->forge->addField([
+            'id'          => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true,],
+            'name'       => ['type' => 'varchar', 'constraint' => '255'],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('category');
+
         $this->forge->addField([
             'id'          => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true,],
             'title'       => ['type' => 'varchar', 'constraint' => '255'],
             'document'    => ['type' => 'varchar', 'constraint' => '255'],
-            'category'    => ['type' => 'varchar', 'constraint' => '255'],
+            'id_category'    => ['type' => 'varchar', 'constraint' => '255'],
             'created_at'  => ['type' => 'datetime'],
             'updated_at'  => ['type' => 'datetime'],
-          ]);
-          $this->forge->addPrimaryKey('id');
-          $this->forge->createTable('archive');
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('id_category', 'category', 'id', '', 'CASCADE');
+        $this->forge->createTable('archive');
     }
 
     public function down()
     {
         $this->forge->dropTable('archive');
+        $this->forge->dropTable('category');
     }
 }
