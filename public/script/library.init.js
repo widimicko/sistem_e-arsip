453b5c1a8f84e7bbc90ae4645bdf9f444f671810
Micko Widi <widimicko85@gmail.com>
2021-12-10 13:08:47 +0700

    change datatable lang to IND

--- public/script/library.init.js
+++ public/script/library.init.js
@@ -1,4 +1,25 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    "language": {
+      "decimal":        "",
+      "emptyTable":     "Tidak ada data yang tersedia",
+      "info":           "Menampilkan _START_ ke _END_ dari _TOTAL_ data",
+      "infoEmpty":      "Menampilkan 0 ke 0 dari 0 data",
+      "infoFiltered":   "(Berhasil memfilter dari _MAX_ data)",
+      "infoPostFix":    "",
+      "thousands":      ",",
+      "lengthMenu":     "Tampilkan _MENU_ data",
+      "loadingRecords": "Memuat...",
+      "processing":     "Memproses...",
+      "search":         "Pencarian:",
+      "zeroRecords":    "Tidak ditemukan data yang cocok",
+      "paginate": {
+        "first":      "Pertama",
+        "last":       "Terakhir",
+        "next":       "Selanjutnya",
+        "previous":   "Sebelumnya"
+      },
+    }
+  });
 });
